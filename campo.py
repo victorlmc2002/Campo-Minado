9cc8f46924fa8cb24e191b2e9fca97621b43141c
Victor Loureiro <victorlmc2002@gmail.com>
2024-08-11 22:03:50 -0300

    jogo finalizado

--- campo.py
+++ campo.py
@@ -1,19 +1,104 @@
-from tkinter import Button
+from tkinter import Button, Label
+import random
+import settings
+import ctypes
+import sys
+
 class Campo:
-    def __init__(self, flag_mina = False):
+    todos = []
+    criado = False
+    qtd_minas = 0
+    qtd_campos = settings.QTD_CAMPOS
+    contador = None
+
+    def __init__(self,x, y, flag_mina = False):
         self.flag_mina= flag_mina
         self.botao = None
+        self.x = x
+        self.y = y
+        self.ja_verificado = False
+
+        Campo.todos.append(self)
 
     def cria_botao(self, posicao):
-        btn = Button(posicao,width= 12, height= 4, text= "teste")
+        btn = Button(posicao,width= 8, height= 2)
         btn.bind('<Button-1>', self.checar_campo)
         btn.bind('<Button-3>', self.marcar_campo)
         self.botao = btn
 
-    def checar_campo(self, event):
-        print(event)
-        print("Esquerda")
+    @staticmethod    
+    def cria_contadores(posicao):
+        contador = Label(posicao, bg= "black", fg= "white",
+                          text= f"Falta {settings.QTD_CAMPOS} campos", font= ("", 30))
+        Campo.contador = contador
     
+    def checar_campo(self, event=None):
+        if Campo.criado == False:
+            Campo.cria_minas()
+            if self.flag_mina:
+                    Campo.qtd_minas -= 1
+            self.flag_mina = False
+            for campo in self.minas_proximas:
+                if campo.flag_mina:
+                    Campo.qtd_minas -= 1
+                campo.flag_mina = False
+            Campo.criado = True
+        if self.ja_verificado:
+            return
+        if self.flag_mina:
+            self.perdeu_jogo()
+        else:
+            Campo.qtd_campos -= 1
+            self.ja_verificado = True
+            if self.qtd_minas_prox == 0:
+                for campo in self.minas_proximas:
+                    campo.checar_campo()
+            self.botao.configure(text=f"{self.qtd_minas_prox}")
+            self.botao.configure(bg= "azure3")
+            if(Campo.contador and Campo.qtd_minas):
+                Campo.contador.configure(text= f"Falta {Campo.qtd_campos} campos\n{Campo.qtd_minas} minas")
+
+    def obtem_campo_coordenadas(self, x, y):
+        for campo in Campo.todos:
+            if campo.x == x and campo.y == y:
+                return campo
+
+    @property
+    def minas_proximas(self):
+        campos_rodeando = [self.obtem_campo_coordenadas(self.x - 1, self.y - 1),
+                           self.obtem_campo_coordenadas(self.x - 1, self.y),
+                           self.obtem_campo_coordenadas(self.x - 1, self.y + 1),
+                           self.obtem_campo_coordenadas(self.x, self.y - 1),
+                           self.obtem_campo_coordenadas(self.x + 1, self.y - 1),
+                           self.obtem_campo_coordenadas(self.x + 1, self.y),
+                           self.obtem_campo_coordenadas(self.x + 1, self.y + 1),
+                           self.obtem_campo_coordenadas(self.x, self.y +1)]
+        campos_rodeando = [campo for campo in campos_rodeando if campo is not None]
+        return campos_rodeando
+    @property
+    def qtd_minas_prox(self):
+        minas = 0
+        for campo in self.minas_proximas:
+            if campo.flag_mina:
+                minas += 1
+        return minas
+
+    def perdeu_jogo(self):
+        self.botao.configure(bg= "red")
+        ctypes.windll.user32.MessageBoxW(0, 'BOOM!', "GAME OVER", 0)
+        sys.exit()
+
     def marcar_campo(self, event):
-        print(event)
-        print("Direita")
+        if self.botao.cget("bg") == "orange":
+            self.botao.configure(bg= "white")
+        else:
+            self.botao.configure(bg= "orange")
+
+    def cria_minas():
+        minas = random.sample(Campo.todos,settings.QTD_MINAS)
+        for mina in minas:
+            mina.flag_mina = True
+            Campo.qtd_minas += 1
+
+    def __repr__(self):
+        return f"Campo ({self.x},{self.y}) {self.flag_mina}"
--- main.py
+++ main.py
@@ -4,32 +4,35 @@ import calculos
 from campo import Campo
 
 root = Tk()
-root.configure(bg= "grey")
+root.configure(bg= "black")
 root.geometry(f'{settings.LARGURA}x{settings.ALTURA}')
 root.title("Campo Minado")
 root.resizable(False, False)
 
-titulo_frame = Frame(root, bg= "grey",
+titulo_frame = Frame(root, bg= "black",
                       width= settings.LARGURA, 
                       height= calculos.definir_altura(20))
 titulo_frame.place(x = 0,y = 0)
-
-contador_frame = Frame(root, bg= "grey",
+titulo = Label(titulo_frame, bg= "black", fg= "white", text= 'Campo Minado', font= ("", 70))
+titulo.place(x= 320, y= 30)
+contador_frame = Frame(root, bg= "black",
                         width= calculos.definir_largura(25), 
                         height= calculos.definir_altura(75))
 contador_frame.place(x = 0, y = calculos.definir_altura(20))
 
-campo_frame = Frame(root, bg= "grey",
+campo_frame = Frame(root, bg= "black",
                     width= calculos.definir_largura(75), 
                     height= calculos.definir_altura(75))
 campo_frame.place(x = calculos.definir_largura(25), y = calculos.definir_altura(20))
 
-for x in range(settings.TAMANHO_CAMPO):
-    for y in range(settings.TAMANHO_CAMPO):
-        c1 = Campo()
+for y in range(settings.TAMANHO_CAMPO):
+    for x in range(settings.TAMANHO_CAMPO):
+        c1 = Campo(x, y)
         c1.cria_botao(campo_frame)
         c1.botao.grid(column= x, row= y)
 
+Campo.cria_contadores(contador_frame)
+Campo.contador.place(x= 0, y= 0)
 
 #Roda a aba
 root.mainloop()
